53f6226c94f0bf50bd39bc5e334b0342e7914624
Andy Godish <agodish18@gmail.com>
2024-03-16 17:42:23 -0600

    fix: added storageaccount resource

--- main.tf
+++ main.tf
@@ -6,5 +6,20 @@ module "resourcegroup" {
   app_prefix = var.app_prefix
   location   = var.location
   sub_prefix = var.sub_prefix
+
+  tags       = var.tags
+}
+
+module "storageaccount" {
+  source  = "app.terraform.io/andygio/storageaccount/azure"
+  version = "1.0.1"
+
+  resource_group_name = module.resourcegroup.rg_name
+
+  app_env    = var.app_env
+  app_prefix = var.app_prefix
+  location   = var.location
+  sub_prefix = var.sub_prefix
+  
   tags       = var.tags
 }
