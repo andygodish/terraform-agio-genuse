80c05d417055b740c7a09b8c75c1fa1fe6ce5b86
Andy Godish <agodish18@gmail.com>
2024-03-16 13:04:17 -0600

    fix: bumped version of resource group module

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "resourcegroup" {
   source  = "app.terraform.io/andygio/resourcegroup/azure"
-  version = "1.0.1"
-  
-  app_env = var.app_env
+  version = "1.0.2"
+
+  app_env    = var.app_env
+  app_prefix = var.app_prefix
+  location = var.location
+  sub_prefix = var.sub_prefix
+  tags = var.tags
 }
